ea8f686a13509325bae16e8f603d443a59a38bc8
zyy <121410280+Zielzz@users.noreply.github.com>
2025-02-10 10:43:38 +0700

    Initial commit

---
+++ Person_2A.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tugas1_Minggu2;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Person_2A {
+    
+      // Declare variables for the person's name and age
+    private String name;
+    private int age;
+
+    // Constructor to initialize the name and age
+    public Person_2A(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Method to return the person's name
+    public String getName() {
+        return name;
+    }
+
+    // Method to return the person's age
+    public int getAge() {
+        return age;
+    }
+
+    // Method to set the person's age
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    
+}
